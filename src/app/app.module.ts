1046d96c3d603cdd9a2f13a454a4419fa60009cd
Guest User <Guest@Epicodus-3D.local>
2018-04-10 11:11:31 -0700

    configure app.module imports and modules and service providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { environment } from '../environments/environment';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
 import { GalleryComponent } from './gallery/gallery.component';
@@ -9,6 +15,12 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
 import { UploadComponent } from './upload/upload.component';
 
+import { AuthenticationGuard } from './services/authenticationGuard.service';
+import { AuthenticationService } from './services/authentication.service';
+import { ImageService } from './services/image.service';
+import { UploadService } from './services/authenticationGuard.service';
+
+import { appRoutes } from './routes';
 
 @NgModule({
   declarations: [
@@ -20,9 +32,19 @@ import { UploadComponent } from './upload/upload.component';
     UploadComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [
+    AuthenticationGuard,
+    AuthenticationService,
+    ImageService,
+    UploadService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { GalleryComponent } from './app/gallery/gallery.component';
+import { ImageDetailComponent } from './app/image-detail/image-detail.component';
+import { LoginComponent } from './app/login/login.component';
+import { UploadComponent } from './app/upload/upload.component';
+import { AuthenticationGuard } from './app/services/authenticationGuard.service';
+
+export const appRoutes: Routes = [
+  { path: 'gallery', component: GalleryComponent, canActivate: [AuthenticationGuard]},
+  { path: 'upload', component: UploadComponent, canActivate: [AuthenticationGuard]},
+  { path: 'image/:id', component: ImageDetailComponent, canActivate: [AuthenticationGuard]},
+  { path: '', redirectTo: '/gallery', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent}
+];
