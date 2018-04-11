1c974b8e41a603f23dad9ebe5995fbe05e16f879
Guest User <Guest@Epicodus-3E.local>
2018-04-11 14:30:13 -0700

    resolve merge conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,11 +28,8 @@ import { UploadService } from './services/upload.service';
 import { AnalysisService } from './services/analysis.service';
 
 import { appRoutes } from './routes';
-<<<<<<< HEAD
 import { AnalysisComponent } from './analysis/analysis.component';
-=======
 import { WebcamComponent } from './webcam/webcam.component';
->>>>>>> 750403306ab66042cfa50f69e75c18b14f04731a
 
 @NgModule({
   declarations: [
@@ -42,11 +39,8 @@ import { WebcamComponent } from './webcam/webcam.component';
     NavbarComponent,
     LoginComponent,
     UploadComponent,
-<<<<<<< HEAD
     AnalysisComponent
-=======
     WebcamComponent
->>>>>>> 750403306ab66042cfa50f69e75c18b14f04731a
   ],
   imports: [
     BrowserModule,
